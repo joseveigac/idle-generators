e12759eb8e9fff65672ebe648feec7323cff5964
Jose Veiga <jveigacaminos@gmail.com>
2026-07-10 19:58:47 +0200

    feat: cloth-config (hudEnabled, dropContentsOnBreak, productionSpeedMultiplier)

--- common/src/main/java/com/ghozix/idlegenerators/IdleGenerators.java
+++ common/src/main/java/com/ghozix/idlegenerators/IdleGenerators.java
@@ -1,9 +1,13 @@
 package com.ghozix.idlegenerators;
 
+import com.ghozix.idlegenerators.config.IGConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+
 public final class IdleGenerators {
     public static final String MOD_ID = "idlegenerators";
 
     public static void init() {
-        // Write common init code here.
+        AutoConfig.register(IGConfig.class, GsonConfigSerializer::new);
     }
 }
--- neoforge/src/main/java/com/ghozix/idlegenerators/neoforge/IdleGeneratorsNeoForge.java
+++ neoforge/src/main/java/com/ghozix/idlegenerators/neoforge/IdleGeneratorsNeoForge.java
@@ -1,13 +1,23 @@
 package com.ghozix.idlegenerators.neoforge;
 
-import net.neoforged.fml.common.Mod;
-
 import com.ghozix.idlegenerators.IdleGenerators;
+import com.ghozix.idlegenerators.config.IGConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(IdleGenerators.MOD_ID)
 public final class IdleGeneratorsNeoForge {
-    public IdleGeneratorsNeoForge() {
+    public IdleGeneratorsNeoForge(ModContainer container) {
         // Run our common setup.
         IdleGenerators.init();
+
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            container.registerExtensionPoint(IConfigScreenFactory.class,
+                    (mc, parent) -> AutoConfig.getConfigScreen(IGConfig.class, parent).get());
+        }
     }
 }
